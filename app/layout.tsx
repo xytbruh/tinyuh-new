36886b3c5b5e6cf6256956eaf0b8fc9c48fdb829
Dimas Nugraha <127030192+xytbruh@users.noreply.github.com>
2024-07-27 23:31:10 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "TINYUH",
   description: "tinyUH also plays economic activities as part of the show. tinyUH reconstructs the whole atmosphere of taste, visual, aroma, sound, up to the trading process, based on the long history of colonialism in Indonesia that intersects through spices, coffee, tea and clay.",
+  keywords: "tinyuh.com tinyuh tiny ",
 };
 
 export default function RootLayout({
